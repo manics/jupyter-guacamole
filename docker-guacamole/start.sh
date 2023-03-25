bfc5c83415db6745524cbf1642593f0dcc59a485
Simon Li <orpheus+devel@gmail.com>
2023-03-25 23:37:27 +0000

    Use Jetty 10 for guacamole client

--- docker-guacamole/start.sh
+++ docker-guacamole/start.sh
@@ -7,6 +7,6 @@ guacd-port: ${GUACD_PORT:-4822}
 EOF
 
 export GUACAMOLE_HOME=/etc/guacamole
-export CATALINA_HOME=/opt/tomcat
 
-exec jhsingle-native-proxy --debug --logs --authtype none --request-timeout 15 --destport 8080 -- /opt/tomcat/bin/catalina.sh run
+exec jhsingle-native-proxy --debug --logs --authtype none --request-timeout 60 --ready-timeout 60 --progressive --destport 8080 -- java -jar $JETTY_HOME/start.jar --debug
+# exec java -jar $JETTY_HOME/start.jar
