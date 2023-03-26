78b76c9ffbb7d1e9a36066f937e75b80cd5b5bd1
Simon Li <orpheus+devel@gmail.com>
2023-03-26 22:34:27 +0100

    ubuntu-mate: disable Firefox sandboxing

--- docker-ubuntu-mate/start-mate.sh
+++ docker-ubuntu-mate/start-mate.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# Firefox sandboxing may not work in a container
+# https://wiki.mozilla.org/Security/Sandbox#Environment_variables
+export MOZ_DISABLE_CONTENT_SANDBOX=1
+export MOZ_DISABLE_GMP_SANDBOX=1
+export MOZ_DISABLE_RDD_SANDBOX=1
+export MOZ_DISABLE_SOCKET_PROCESS_SANDBOX=1
+
 exec dbus-launch mate-session "$@"
