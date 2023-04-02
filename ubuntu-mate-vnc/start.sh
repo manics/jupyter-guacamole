8bb50dbb4901f2d7139831a8e78f5e2862482b71
Simon Li <orpheus+devel@gmail.com>
2023-04-02 23:03:42 +0100

    Copy ~ubuntu so it can be restored if new volume is mounted

--- ubuntu-mate-vnc/start.sh
+++ ubuntu-mate-vnc/start.sh
@@ -12,4 +12,9 @@ else
   exit 1
 fi
 
+if [ ! -d /home/ubuntu/Desktop ]; then
+  echo "First run, setting up ubuntu"
+  rsync /home/ubuntu.orig/ -av --ignore-existing /home/ubuntu/
+fi
+
 /usr/bin/tigervncserver :1 -fg $LOCALHOST_ARGS -SecurityTypes None -xstartup /usr/local/bin/start-mate.sh
